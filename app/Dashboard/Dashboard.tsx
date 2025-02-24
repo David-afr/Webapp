dd2dc80447904029191b7cc13f08a55660b07629
david-afr <david.afr.28@gmail.com>
2025-02-24 05:51:18 -0400

    First preview 5

--- app/Dashboard/Dashboard.tsx
+++ app/Dashboard/Dashboard.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
-import Carousel from "../components/Carousel/Carousel";
+
 import "./dashboard.css";
 import Button from '../components/Button/Button';
 
