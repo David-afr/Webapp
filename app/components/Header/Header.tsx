1dad13dfe86dcc5b2ccdd1098175c6ff2683df77
Jonathan Urdaneta <jonathan.urdaneta@codelogicsoftware.com>
2025-04-12 00:57:13 -0400

    feat: add toggle icons for mobile menu in Header component

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -77,8 +77,12 @@ const Header = () =>
 
                 <div className="relative block sm:hidden" id="menu-button">
                     <button className="bg-white rounded shadow-lg p-1" onClick={toggleMenu}>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-[26px] h-[26px] text-celeste font-bold">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`w-[26px] h-[26px] text-celeste font-bold ${showMenu ? 'hidden' : 'block'}`}>
                             <path fillRule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clipRule="evenodd"></path>
+                        </svg>
+
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={`w-[26px] h-[26px] text-celeste font-bold ${showMenu ? 'block' : 'hidden'}`}>
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
                         </svg>
                     </button>
                 </div>
