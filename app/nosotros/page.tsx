f1e630b035cc41034f07818fb2cf39a8655c6fcf
david-afr <david.afr.28@gmail.com>
2025-04-13 00:35:53 -0400

    refactor: improve layout and responsiveness in Page component

--- app/nosotros/page.tsx
+++ app/nosotros/page.tsx
@@ -2,8 +2,8 @@ export default function Page() {
     return (
         <>
             {/* Inicio */}
-            <div className="pt-initPage bg-nosotros flex justify-center h-[78vh]">
-                <div className="max-w-[50%] text-[3.8vw] sm:text-semisubtitulo py-initSectionY">
+            <div className="pt-initPage bg-nosotros flex justify-center h-[55vh] sm:h-[78vh]">
+                <div className="max-w-[75%] sm:max-w-[50%] pt-[50px] sm:pt-0 text-[3.8vw] sm:text-semisubtitulo py-initSectionY">
                     <div className="font-light text-center">
                         <p className="text-white">
                             HHO Renting es una empresa con más de 14 años de experiencia, dedicada a desarrollar soluciones innovadoras con impacto social. A través de su 
@@ -23,7 +23,7 @@ export default function Page() {
                 </div>
 
                 {/* Text */}
-                <div className="mt-[40px] max-w-[60%] text-justify">
+                <div className="mt-[40px] max-w-[95%] sm:max-w-[60%] text-justify">
                     <p className="text-gray-800 text-xl">
                     HHO Renting S.A.S. es una empresa colombiana constituida en el 2014 en la ciudad de Cali, Valle del Cauca; inició sus operaciones enfocada en impulsar 
                     soluciones sostenibles a través de un modelo innovador, orientado a la instalación de generadores de combustible limpio HHO en diversos tipos de vehículos. 
@@ -60,19 +60,19 @@ export default function Page() {
 
             {/* Mision/Vision */}
             <div className="bg-celeste py-initSectionY px-iniSectionX">
-                <div className="w-full flex justify-center">
+                <div className="w-full py-[30px] sm:py-0 flex flex-col sm:flex-row items-center sm:items-start justify-center">
                     {/* Mision */}
-                    <div className="text-center mr-[60px] w-[40%]">
+                    <div className="text-center sm:mr-[60px] w-full sm:w-[40%]">
                         <h3 className="text-white font-titulo text-[8vw] sm:text-titulo">Misión</h3>
-                        <p className="text-white text-xl text-justify">Somos un servicio de financiación inclusivo que facilita a los clientes residenciales de empresas de servicios públicos 
+                        <p className="text-white text-xl text-justify pt-3 sm:pt-0">Somos un servicio de financiación inclusivo que facilita a los clientes residenciales de empresas de servicios públicos 
                                                             la adquisición de productos y servicios mediante créditos fáciles, rápidos y seguros. Nuestro objetivo es mejorar la 
                                                             calidad de vida de las familias, reconociendo su buen comportamiento de pago en sus facturas de servicios públicos.</p>
                     </div>
 
                     {/* Vision */}
-                    <div className="text-center w-[40%]">
+                    <div className="pt-[60px] sm:pt-0 text-center w-full sm:w-[40%]">
                         <h3 className="text-white font-titulo text-[8vw] sm:text-titulo">Visión</h3>
-                        <p className="text-white text-xl text-justify">Para 2030, nos posicionaremos como un líder en el sector de financiación inclusiva, alcanzando una participación 
+                        <p className="text-white text-xl text-justify pt-3 sm:pt-0">Para 2030, nos posicionaremos como un líder en el sector de financiación inclusiva, alcanzando una participación 
                                                             significativa en el mercado de créditos para clientes residenciales. Nos comprometemos a ser reconocidos por nuestra 
                                                             innovación en productos financieros accesibles y por mejorar la calidad de vida de las familias, estableciendo 
                                                             relaciones de confianza y ofreciendo soluciones que fomenten el bienestar económico y social de nuestros clientes.</p>
@@ -88,7 +88,7 @@ export default function Page() {
                 </div>
 
                 {/* Map */}
-                <div className="mt-[40px] max-w-[85%] rounded-[30px] overflow-hidden">
+                <div className="mt-[40px] w-full sm:max-w-[85%] rounded-lg sm:rounded-[30px] overflow-hidden">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3982.5370391976744!2d-76.53040969999999!3d3.4620317!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e30a7007140f3e7%3A0x15918d49225fcf11!2sEdificio%20Centro%2020!5e0!3m2!1sen!2sve!4v1743414711575!5m2!1sen!2sve" width="1060" height="560" loading="lazy"></iframe>
                 </div>
             </div>
@@ -101,7 +101,7 @@ export default function Page() {
                 </div>
 
                 {/* Text */}
-                <div className="mt-[40px] max-w-[60%]">
+                <div className="mt-[40px] max-w-[95%] sm:max-w-[60%]">
                     <p className="text-center text-white text-xl">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur reiciendis praesentium voluptas modi. Nemo excepturi debitis, commodi iure animi laudantium sit repellendus rerum facilis hic perspiciatis? Odio libero ad a.
                     </p>
